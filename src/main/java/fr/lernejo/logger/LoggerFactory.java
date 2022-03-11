912eb0acb4c3655f6afcc8a82a2405fd917d4425
Clarel Salassa <clarelsalassapro@gmail.com>
2022-03-11 11:42:56 +0100

    Partie 7

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,8 +1,13 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
+
     public static Logger getLogger(String name){
+        Predicate<String> condition = message->message.contains("simulation");
         return new ContextualLogger(name,
-            new CompositeLogger(new ConsoleLogger(), new FileLogger("logs")));
+            new CompositeLogger(new ConsoleLogger(),
+                                new FilteredLogger(new FileLogger("logs"), condition)));
     }
 }
