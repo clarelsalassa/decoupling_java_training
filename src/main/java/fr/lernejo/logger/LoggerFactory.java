1e5440869ac184832eaaee9ed08e34b49341b103
Clarel Salassa <clarelsalassapro@gmail.com>
2022-03-11 11:26:47 +0100

    Partie 6

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,6 +2,7 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name){
-        return new ContextualLogger(name, new FileLogger("logs"));
+        return new ContextualLogger(name,
+            new CompositeLogger(new ConsoleLogger(), new FileLogger("logs")));
     }
 }
